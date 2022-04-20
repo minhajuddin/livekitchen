55e2568b6ed3033329ae1b23e04e65f179f80f89
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2022-04-20 09:54:33 -0400

    Tweak host

--- set-pixels.rb
+++ set-pixels.rb
@@ -1,7 +1,9 @@
 require 'json'
 require 'excon'
 img = JSON.parse(ARGF.read) #.shuffle
-cx = Excon.new('http://localhost:4010/', :persistent => true)
+# host = 'http://localhost:4010/'
+host = "https://entidplace.fly.dev"
+cx = Excon.new(host, :persistent => true)
 img.map{|pixel| {method: 'POST', path: '/api/pixel', persistent: true, :body => pixel.merge(player: "Mujju").to_json, :headers => {'Content-Type' => 'application/json'}}}.each_slice(100).to_a.shuffle.each do |reqs|
   cx.batch_requests(reqs)
 end
