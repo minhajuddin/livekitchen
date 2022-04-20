5f1bfd7078ae42f9e2f0c5dd662d2f4c716becfa
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2022-04-20 14:08:17 -0400

    Save

--- set-pixels.rb
+++ set-pixels.rb
@@ -5,7 +5,8 @@ player = ENV['PLAYER'] || "ruby"
 host = 'http://localhost:4010/'
 # host = "https://entidplace.fly.dev"
 cx = Excon.new(host, :persistent => true)
-img.map{|pixel| {method: 'POST', path: '/api/pixel', persistent: true, :body => pixel.merge(player: player).to_json, :headers => {'Content-Type' => 'application/json'}}}.each_slice(100).to_a.shuffle.each do |reqs|
+img.map{|pixel| {method: 'POST', path: '/api/pixel', persistent: true, :body => pixel.merge(player: player).to_json, :headers => {'Content-Type' => 'application/json'}}}.each_slice(10).to_a.shuffle.each do |reqs|
   puts cx.batch_requests(reqs).map{|x| x.status}
+  sleep 1
   puts "----------------------------------------"
 end
