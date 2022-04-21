1626912d67086e359b4e1eae76064aa129f5b21d
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2022-04-21 16:26:39 -0400

    Tweak pixel setter

--- set-pixels.rb
+++ set-pixels.rb
@@ -2,11 +2,11 @@ require 'json'
 require 'excon'
 img = JSON.parse(ARGF.read) #.shuffle
 player = ENV['PLAYER'] || "ruby"
-host = 'http://localhost:4010/'
-# host = "https://entidplace.fly.dev"
-cx = Excon.new(host, :persistent => true)
-img.map{|pixel| {method: 'POST', path: '/api/pixel', persistent: true, :body => pixel.merge(player: player).to_json, :headers => {'Content-Type' => 'application/json'}}}.each_slice(10).to_a.shuffle.each do |reqs|
-  puts cx.batch_requests(reqs).map{|x| x.status}
+# host = "http://localhost:4010"
+host = "https://entidplace.fly.dev"
+cx = Excon.new(host, persistent: true, debug: false)
+img.reject{|x| x['color'] == "rgba(0, 0, 0, 0)" ||  x['color'] == "rgba(255, 255, 255, 1)"}.map{|pixel| {method: 'POST', path: '/api/pixel', persistent: true, :body => pixel.merge(player: player).to_json, :headers => {'Content-Type' => 'application/json'}}}.each_slice(10).to_a.shuffle.each do |reqs|
+  print "."
+  cx.batch_requests(reqs).map{|x| x.status}
   sleep 1
-  puts "----------------------------------------"
 end
